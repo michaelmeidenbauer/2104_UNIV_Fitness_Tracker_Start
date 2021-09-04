9455571dc066fe8623c14576dc3abcee47f26739
trb1216 <trb1216@gmail.com>
2021-09-04 03:22:29 -0500

    from stash

--- api/index.js
+++ api/index.js
@@ -1,3 +1,29 @@
 // create an api router
+const express = require("express");
+const apiRouter = express.Router();
+
+// health check
+// GET /health
+
+apiRouter.get("/health", (req, res) => {
+  res.send("All is well.");
+});
+
 // attach other routers from files in this api directory (users, activities...)
+const usersRouter = require("./users");
+apiRouter.use("/users", usersRouter);
+
+const activitiesRouter = require("./activities");
+apiRouter.use("/activities", activitiesRouter);
+
+const routinesRouter = require("./routines");
+apiRouter.use("/routines", routinesRouter);
+
+const routine_activitiesRouter = require("./routine_activities");
+apiRouter.use("/routine_activities", routine_activitiesRouter);
+
+apiRouter.use((error, req, res, next) => {
+  res.send(error);
+});
 // export the api router
+module.exports = apiRouter;
--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // create the express server here
+var express = require('express');
+var cors = require('cors');
+var app = express();
+const apiRouter = express.Router();
+
+app.use(cors())
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(3000, function () {
+  console.log('CORS-enabled web server listening on port 5432')
+})
